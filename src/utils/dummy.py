8baaeb794b2e3f29c464bc2bdc134e3e8fdeee53
ombayley <150044883+ombayley@users.noreply.github.com>
2025-12-05 09:57:51 +0100

    testing script to summarise run log data

---
+++ src/utils/dummy.py
@@ -0,0 +1,33 @@
+import json
+import math
+import pandas as pd
+
+
+def main():
+    path = r"C:\Users\nrg_lcms\ChromTroller\src\log_files\run_logs\2025_12_04\RunLog_2025_12_04-16_19_58.json"
+    with open(path) as f:
+        data: list[dict] = json.load(f)
+    prod_rt = 5.0
+    istd_rt = 5.9
+    tol = 0.05
+    raw_integrals: list[dict] = []
+    for item in data:
+        run_dict = {"id": item.get("run_conditions").get("run_id"), "prod": 0, "istd": 0}
+        peaks = item.get("analysis").get("peak_rt")
+        if peaks is not None:
+            for key, value in peaks.items():
+                if math.isclose(value, prod_rt, abs_tol=tol):
+                    run_dict["prod"] = item.get("analysis").get("integral")[key]
+                if math.isclose(value, istd_rt, abs_tol=tol):
+                    run_dict["istd"] = item.get("analysis").get("integral")[key]
+            raw_integrals.append(run_dict)
+
+    integral_df = pd.DataFrame.from_dict(raw_integrals)
+    integral_df.sort_values(by="id", ascending=True, inplace=True)
+    integral_df.reset_index(drop=True, inplace=True)
+
+    path = r"C:\Users\nrg_lcms\ChromTroller\src\log_files\run_logs\2025_12_04\RunLog_2025_12_04-16_19_58.csv"
+    integral_df.to_csv(path)
+
+if __name__ == "__main__":
+    main()
